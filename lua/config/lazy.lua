89d40134c12c427a9b4df2814d18865967fa35f1
Cédric Prezelin <cedric.prezelin-ext@navocap.com>
2024-11-28 16:00:15 +0100

    feat(extras): add mini-surround

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.test.core" },
     -- import/override with your plugins
