731487ee9a45c4cd23c993a8a08e569e3f8798f8
Cédric Prezelin <ext-cedric.prezelin@showroomprive.net>
2025-02-07 19:27:31 +0100

    feat(extras): add snacks picker

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.omnisharp" },
+    { import = "lazyvim.plugins.extras.editor.snacks_picker" },
     -- LazyExtras configuration
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.dap.core" },
