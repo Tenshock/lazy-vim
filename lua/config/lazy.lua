e476efce7340ceb8b9698a653284119402fabfe7
Cédric Prezelin <ext-cedric.prezelin@showroomprive.net>
2025-02-07 22:35:57 +0100

    feat(extras): add nix lang support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.nix" },
     { import = "lazyvim.plugins.extras.lang.omnisharp" },
     { import = "lazyvim.plugins.extras.editor.snacks_picker" },
     -- LazyExtras configuration
