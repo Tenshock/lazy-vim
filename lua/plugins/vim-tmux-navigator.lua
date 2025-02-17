af96c1d2cdd41246375eba505a2cfe4e72c73a84
Cédric Prezelin <ext-cedric.prezelin@showroomprive.net>
2025-02-17 11:32:54 +0100

    chore(nvim): harmonise split and navigation

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -10,16 +10,16 @@ return {
   },
   keys = {
     -- Normal & Insert Mode Mappings
-    { "<M-Down>", "<cmd>TmuxNavigateDown<cr>" },
-    { "<M-Left>", "<cmd>TmuxNavigateLeft<cr>" },
-    { "<M-Up>", "<cmd>TmuxNavigateUp<cr>" },
-    { "<M-Right>", "<cmd>TmuxNavigateRight<cr>" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
+    { "<C-l>", "<cmd>TmuxNavigateLeft<cr>" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { "<C-h>", "<cmd>TmuxNavigateRight<cr>" },
     { "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
 
     -- Visual Mode Fix: Ensure It Works Without Pressing Twice
-    { "<M-Down>", "<Esc><cmd>TmuxNavigateDown<CR>gv", mode = "v" },
-    { "<M-Left>", "<Esc><cmd>TmuxNavigateLeft<CR>gv", mode = "v" },
-    { "<M-Up>", "<Esc><cmd>TmuxNavigateUp<CR>gv", mode = "v" },
-    { "<M-Right>", "<Esc><cmd>TmuxNavigateRight<CR>gv", mode = "v" },
+    { "<C-j>", "<Esc><cmd>TmuxNavigateDown<CR>gv", mode = "v" },
+    { "<C-l>", "<Esc><cmd>TmuxNavigateLeft<CR>gv", mode = "v" },
+    { "<C-k>", "<Esc><cmd>TmuxNavigateUp<CR>gv", mode = "v" },
+    { "<C-h>", "<Esc><cmd>TmuxNavigateRight<CR>gv", mode = "v" },
   },
 }
