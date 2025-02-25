e0a274b5489828ddea61831d06c366405347a32f
Cédric Prezelin <cedric.prezelin@gmail.com>
2025-02-25 11:25:12 +0100

    feat: configure csharpier to use nix-installed binary
    .NET installation is isolated in /nix/store so the pre-built csharpier package installed by Mason does not access it.

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters = {
+      csharpier = {
+        command = "/etc/profiles/per-user/cedric/bin/dotnet-csharpier",
+      },
+    },
+  },
+}
