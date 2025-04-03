f1482368ba5b7ed6b8dd882d72361da466ac0dff
Cédric Prezelin <cedric.prezelin@gmail.com>
2025-04-03 14:53:40 +0200

    feat: update git highlights

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,14 @@
 return {
-  { "marko-cerovac/material.nvim" },
+  {
+    "marko-cerovac/material.nvim",
+    opts = {
+      custom_highlights = {
+        -- DiffAdd = { bg = "#f9e2af" }, -- YELLOW
+        DiffDelete = { bg = "#550000", fg = "#550000" },
+        -- DiffText = { bg = "#444400" }, -- BLUE
+      },
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
