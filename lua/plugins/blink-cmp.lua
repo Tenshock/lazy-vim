9b7df1bc75c3020861b292567f31b90ac19dd66c
Cédric Prezelin <cedric.prezelin@gmail.com>
2025-04-18 10:33:58 +0200

    feat(blink): prefer rust with authorized download

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -2,12 +2,19 @@ return {
   "saghen/blink.cmp",
   opts = function(_, opts)
     opts.sources = {
-      transform_items = function(ctx, items)
+      transform_items = function(_, items)
         -- Remove the "Text" source from lsp autocomplete
         return vim.tbl_filter(function(item)
           return item.kind ~= vim.lsp.protocol.CompletionItemKind.Text
         end, items)
       end,
+    }
+
+    opts.fuzzy = {
+      implementation = "prefer_rust_with_warning",
+      prebuilt_binaries = {
+        download = true,
+      },
     }
   end,
 }
