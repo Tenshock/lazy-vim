23736adc6139d9ea4e29e6e926cd2e379b2517a0
Cédric Prezelin <cedric.prezelin@gmail.com>
2025-09-17 17:31:12 +0200

    feat: add refactoring support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,6 +30,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.editor.snacks_picker" },
+    { import = "lazyvim.plugins.extras.editor.refactoring" },
     -- LazyExtras configuration
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.dap.core" },
