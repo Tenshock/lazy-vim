5b0529573f6de478014daddedd25a0d5337de613
Cédric Prezelin <cedric.prezelin@gmail.com>
2026-08-03 10:13:22 +0200

    feat: add french to spell languages

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,5 @@ vim.g.snacks_animate = false
 
 -- Always use the git root for project scope (avoids LSP/package roots in monorepos).
 vim.g.root_spec = { ".git" }
+
+vim.opt.spelllang = { "en", "fr" }
