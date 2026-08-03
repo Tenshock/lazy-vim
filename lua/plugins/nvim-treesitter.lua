e1686b1a7797ad5dafdcac161047d42797cd58c1
Cédric Prezelin <cedric.prezelin@gmail.com>
2026-08-03 11:50:58 +0200

    feat: add typst LSP

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,4 +1,4 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = { ensure_installed = { "scss", "css", "hyprlang" } },
+  opts = { ensure_installed = { "scss", "css", "hyprlang", "typst" } },
 }
