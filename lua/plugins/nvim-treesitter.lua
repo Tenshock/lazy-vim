27f6691f4ca8393db1a89c21b16921738a494081
Cédric Prezelin <cedric.prezelin@gmail.com>
2026-08-08 16:01:54 +0200

    feat: add ini and zsh parsing support

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,4 +1,4 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = { ensure_installed = { "scss", "css", "hyprlang", "typst" } },
+  opts = { ensure_installed = { "scss", "css", "hyprlang", "typst", "ini", "zsh" } },
 }
